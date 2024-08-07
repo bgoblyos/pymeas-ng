a7201c804dbe34070b29e66d111ad88524a33857
Bence Göblyös <bence@goblyos.dev>
2024-08-07 13:40:21 +0200

    Implement overlap for Stitched Sweep

--- Experiments/StitchedSweep.py
+++ Experiments/StitchedSweep.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import QTimer
 from random import uniform
 
 from math import ceil
-from numpy import array, linspace, concatenate, zeros
+from numpy import array, linspace, concatenate, zeros, maximum
 
 import logging
 
@@ -30,9 +30,14 @@ class StitchedSweep():
 
         # Connect start button
         self.ui.StitchedSweepStartButton.clicked.connect(self.prepare)
+        self.ui.StitchedSweepStartButton.setEnabled(True)
 
         # Connect export button
         self.ui.StitchedSweepExportButton.clicked.connect(self.export)
+
+        # Connect cancel button
+        self.ui.StitchedSweepCancelButton.clicked.connect(self.cancel)
+        self.ui.StitchedSweepCancelButton.setEnabled(False)
 
         # Set up timer for progress bar
         # (and for waiting until the sweep is done)
@@ -71,6 +76,26 @@ class StitchedSweep():
     def export(self):
         promptSweepExport(self.freqs, self.data1, self.data2)
 
+    def cancel(self):
+        self.ui.StitchedSweepCancelButton.setEnabled(False)
+
+        self.timer.stop()
+
+        # Disable sweeper output
+        self.sweeper.powerOff()
+
+        self.lockin.pause()
+
+        # Reset sweep progress bar
+        self.ui.StitchedSweepSweepProgress.setRange(0, 1)
+        self.ui.StitchedSweepSweepProgress.setValue(0)
+
+        # Reset total progress bar
+        self.ui.StitchedSweepTotalProgress.setRange(0, 1)
+        self.ui.StitchedSweepTotalProgress.setValue(0)
+
+        self.ui.StitchedSweepStartButton.setEnabled(True)
+
     def display1Toggled(self):
         if self.ui.StitchedSweepDisp1Check.isChecked():
             self.ui.StitchedSweepDisp1Selection.setEnabled(True)
@@ -91,22 +116,40 @@ class StitchedSweep():
             logging.warning("At least one channel must be enabled!")
             return
         
+        if self.channel1:
+            disp = self.ui.StitchedSweepDisp1Selection.currentIndex()
+            self.lockin.setDisplay(1, disp)
+
+        if self.channel2:
+            disp = self.ui.StitchedSweepDisp2Selection.currentIndex()
+            self.lockin.setDisplay(2, disp)
+        
         self.data1 = array([])
         self.data2 = array([])
         self.freqs = array([])
         
         #TODO: Disable start and enable cancel
+        self.ui.StitchedSweepStartButton.setEnabled(False)
+        self.ui.StitchedSweepCancelButton.setEnabled(True)
 
         logging.debug("Started Stitched Sweep measurement")
        
         self.numSweeps = self.ui.StitchedSweepNumSweeps.value()
 
+        self.overlap = self.ui.StitchedSweepOverlap.value() / 100
+        lengthFactor = 1 + self.overlap
+
+        self.discard = self.overlap = self.ui.StitchedSweepDiscard.value()
+
         # Sweeper parameters
         self.sweepTime = self.ui.StitchedSweepSweepTime.value()
         startFreq = self.ui.StitchedSweepStartFreq.value()
         endFreq = self.ui.StitchedSweepEndFreq.value()
-        self.sweepFreqs = linspace(startFreq, endFreq, self.numSweeps + 1)
-        self.sweepCounter = 1
+        self.endFreqs, stepSize = linspace(startFreq, endFreq, self.numSweeps + 1, retstep=True)
+        self.endFreqs = self.endFreqs[1:]
+        self.startFreqs = maximum(self.endFreqs - (stepSize * lengthFactor), startFreq)
+        
+        self.sweepCounter = 0
 
         # Set power level and turn off continous sweep
         self.sweeper.setPowerLevel(self.ui.StitchedSweepPower.value())
@@ -138,8 +181,8 @@ class StitchedSweep():
         self.startRun()
 
     def startRun(self):
-        startFreq = self.sweepFreqs[self.sweepCounter - 1]
-        endFreq = self.sweepFreqs[self.sweepCounter]
+        startFreq = self.startFreqs[self.sweepCounter]
+        endFreq = self.endFreqs[self.sweepCounter]
 
         # TODO: Update these
         self.sweeper.setupSweep(startFreq, endFreq, self.sweepTime)
@@ -179,6 +222,7 @@ class StitchedSweep():
         # Stop the timer so updateProgress stops running
         self.timer.stop()
         logging.info("Sweep completed, extracting data...")
+        
 
         # Set progress bar to indeterminate
         pbar = self.ui.StitchedSweepSweepProgress
@@ -198,12 +242,12 @@ class StitchedSweep():
         logging.debug(f"{self.numPoints} points will be extracted")
         logging.debug(f"The buffer contains {self.lockin.query('SPTS?')}")
         if self.channel1:
-            newdata = array(self.lockin.readBuffer(1, 0, self.numPoints))
+            newdata = array(self.lockin.readBuffer(1, 0, self.numPoints))[self.discard:]
             logging.debug(f"Register 1 extracted")
             self.data1 = concatenate((self.data1, newdata, padding))
 
         if self.channel2:
-            newdata = array(self.lockin.readBuffer(2, 0, self.numPoints))
+            newdata = array(self.lockin.readBuffer(2, 0, self.numPoints))[self.discard:]
             logging.debug(f"Register 2 extracted")
             self.data1 = concatenate((self.data2, newdata, padding))
 
@@ -211,7 +255,7 @@ class StitchedSweep():
 
         # Read back sweep parameters from the instrument
         start, end, _ = self.sweeper.readSweepParams()
-        newfreqs = linspace(start, end, self.numPoints, endpoint=False)
+        newfreqs = linspace(start, end, self.numPoints, endpoint=False)[self.discard:]
         self.freqs = concatenate((self.freqs, newfreqs))
 
         # Plot the data 
@@ -225,12 +269,14 @@ class StitchedSweep():
         pbar.setValue(1)
 
         # Update the total progress
+        self.sweepCounter += 1
         self.ui.StitchedSweepTotalProgress.setValue(self.sweepCounter)
 
         # Run again if we're not done yet
         if self.numSweeps > self.sweepCounter:
-            self.sweepCounter += 1
             self.startRun()
+        else:
+            self.ui.StitchedSweepStartButton.setEnabled(True)
 
 
     def selected(self):
@@ -285,6 +331,22 @@ class StitchedSweep():
         currentFreq = self.lockin.readFreq()
         self.ui.StitchedSweepLockInFreq.setValue(currentFreq)
 
+        # Display 1
+        self.ui.StitchedSweepDisp1Selection.clear()
+        for disp in self.lockin.display1List:
+            self.ui.StitchedSweepDisp1Selection.addItem(disp)
+
+        currentDisplay1 = self.lockin.readDisplay(1)
+        self.ui.StitchedSweepDisp1Selection.setCurrentIndex(currentDisplay1)
+
+        # Display 2
+        self.ui.StitchedSweepDisp2Selection.clear()
+        for disp in self.lockin.display2List:
+            self.ui.StitchedSweepDisp2Selection.addItem(disp)
+
+        currentDisplay2 = self.lockin.readDisplay(2)
+        self.ui.StitchedSweepDisp2Selection.setCurrentIndex(currentDisplay2)
+
         self.updateEstimates()
 
     def updateEstimates(self):
@@ -297,4 +359,4 @@ class StitchedSweep():
 
         points = round(sweepTime*sampleFreq)
 
-        #self.ui.sweepAndLockDataPointsLabel.setText(f"{points} ({maxBins} max)")
+        self.ui.StitchedSweep.setText(f"{points} ({maxBins} max)")
